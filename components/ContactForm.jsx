f6acd5b887b52f4b852f76c5fb00f2ef2841e5a6
Haribhusal <bhusalhari89@gmail.com>
2023-08-05 17:51:35 +0545

    contact form disable

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -29,59 +29,80 @@ const ContactForm = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}
-            class="rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white dark:bg-red-500 -mb-20 md:mt-20 mt-0 ">
+      <form
+        onSubmit={handleSubmit}
+        class="rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white dark:bg-red-500 -mb-20 md:mt-20 mt-0 "
+      >
+        <h1 class="text-2xl font-bold dark:text-gray-50">Send a message</h1>
 
-            <h1 class="text-2xl font-bold dark:text-gray-50">Send a message</h1>
+        <label
+          htmlFor="fullName"
+          class="mb-2 text-gray-500 font-light mt-4 dark:text-gray-50"
+        >
+          Full Name<span class="text-red-500 dark:text-gray-50">*</span>
+        </label>
+        <input
+          required
+          id="fullName"
+          placeholder="Enter your Full Name"
+          value={values.fullName}
+          onChange={handleChange}
+          type="text"
+          className="bg-transparent border-slate-300 rounded-md py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-0 font-light text-gray-500"
+        />
 
-            <label htmlFor="fullName" class="mb-2 text-gray-500 font-light mt-4 dark:text-gray-50">Full Name<span class="text-red-500 dark:text-gray-50">*</span></label>
-            <input
-                required
-                id='fullName'
-                placeholder='Enter your Full Name'
-                value={values.fullName}
-                onChange={handleChange}
-                type='text'
-                className="bg-transparent border-slate-300 rounded-md py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-0 font-light text-gray-500"
-            />
-
-            <label htmlFor="subject" class="mb-2 text-gray-500 font-light mt-4 dark:text-gray-50">Subject<span class="text-red-500 dark:text-gray-50">*</span></label>
-            <input
-                required
-                id='subject'
-                placeholder='Enter Subject'
-                value={values.subject}
-                onChange={handleChange}
-                type='text'
-                className="bg-transparent border-slate-300 rounded-md py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-0 font-light text-gray-500"
-
-            />
-            <label htmlFor="message" class="mb-2 text-gray-500 font-light mt-4 dark:text-gray-50">Your Message<span class="text-red-500">*</span></label>
-            <textarea
-                required
-                placeholder='Enter your Message'
-                value={values.message}
-                onChange={handleChange}
-                id='message'
-                rows={4}
-                className="bg-transparent border-slate-300 rounded-md py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-0 font-light text-gray-500"
-
-            />
-            {responseMessage.message &&
-                <div className="response bg-green-600 text-white px-5 py-3 my-5 rounded-md d-flex items-center leading-10 text-center my">
-                    {responseMessage.message}
-                </div>
-            }
-            <div class="flex flex-row items-center justify-start">
-                <button
-                    disabled={values.email == '' || values.message == '' || values.subject == ""}
-                    onClick={() => setLoading(true)}
-                    type='submit' value='submit' class="px-10 mt-8 py-2 bg-red-700 gap-3 flex items-center text-gray-50 font-light rounded-md text-lg">
-                    Send
-                    <i className={`las ${loading ? "la-spinner animate-spin" : "la-telegram-plane"} `}></i>
-                </button>
-            </div>
-        </form >
+        <label
+          htmlFor="subject"
+          class="mb-2 text-gray-500 font-light mt-4 dark:text-gray-50"
+        >
+          Subject<span class="text-red-500 dark:text-gray-50">*</span>
+        </label>
+        <input
+          required
+          id="subject"
+          placeholder="Enter Subject"
+          value={values.subject}
+          onChange={handleChange}
+          type="text"
+          className="bg-transparent border-slate-300 rounded-md py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-0 font-light text-gray-500"
+        />
+        <label
+          htmlFor="message"
+          class="mb-2 text-gray-500 font-light mt-4 dark:text-gray-50"
+        >
+          Your Message<span class="text-red-500">*</span>
+        </label>
+        <textarea
+          required
+          placeholder="Enter your Message"
+          value={values.message}
+          onChange={handleChange}
+          id="message"
+          rows={4}
+          className="bg-transparent border-slate-300 rounded-md py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-0 font-light text-gray-500"
+        />
+        {responseMessage.message && (
+          <div className="response bg-green-600 text-white px-5 py-3 my-5 rounded-md d-flex items-center leading-10 text-center my">
+            {responseMessage.message}
+          </div>
+        )}
+        <div class="flex flex-row items-center justify-start">
+          <button
+            // disabled={values.email == '' || values.message == '' || values.subject == ""}
+            onClick={() => setLoading(true)}
+            type="submit"
+            value="submit"
+            class="px-10 mt-8 py-2 bg-red-700 gap-3 flex items-center text-gray-50 font-light rounded-md text-lg"
+          >
+            Send
+            <i
+              className={`las ${
+                loading ? "la-spinner animate-spin" : "la-telegram-plane"
+              } `}
+            ></i>
+          </button>
+        </div>
+      </form>
     );
 };
 
